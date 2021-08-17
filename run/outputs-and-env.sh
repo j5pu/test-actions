539e90e1c771c7532bf4c9b1b708b78dcec8a82b
José Antonio Puértolas Montañés <63794670+jose-nferx@users.noreply.github.com>
2021-08-17 05:17:44 +0200

    'auto commit'

---
+++ run/outputs-and-env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+(
+  export GH_CONFIG_DIR="${HOME}/data/config/git"
+  source "${HOME}/data/config/.gitattributes"
+  gh loguser
+  gh workflow run "outputs-and-env".yml
+  sleep 2
+  run_info="$( gh run list --workflow=outputs-and-env.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
+  id="$( echo "${run_info}" | awk '{print $7}' )"
+  gh run watch -i 1 "${id}"
+  run_info="$( gh run list --workflow=outputs-and-env.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
+  status="$( echo "${run_info}" | awk '{print $1}' )"
+  rc="$( echo "${run_info}" | awk '{print $2}' )"
+  echo "status=${status}, rc=${rc} id=${id}"
+  gh run view "${id}"
+  open "https://github.com/j5pu/test-actions/actions/runs/${id}"
+)
