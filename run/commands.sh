c6a21511736f550acc863ad380d1e20799f15404
José Antonio Puértolas Montañés <63794670+jose-nferx@users.noreply.github.com>
2021-08-17 03:09:15 +0200

    'auto commit'

--- run/commands.sh
+++ run/commands.sh
@@ -3,5 +3,14 @@
   export GH_CONFIG_DIR="${HOME}/data/config/git"
   source "${HOME}/data/config/.gitattributes"
   gh loguser
-  gh workflow run "commands"
+  gh workflow run "commands".yml
+  gh run list --workflow="commands".yml
+  gh run watch -i 1 2>/dev/null
+  run_info="$( gh run list --workflow=intro.yml --limit 1 | grep -v "^STATUS" | grep workflow_dispatch )"
+  status="$( echo "${run_info}" | awk '{print $1}' )"
+  rc="$( echo "${run_info}" | awk '{print $2}' )"
+  id="$( echo "${run_info}" | awk '{print $7}' )"
+  echo "status=${status}, rc=${rc} id=${id}"
+  gh run view "${id}"
+  open "https://github.com/j5pu/test-actions/actions/runs/${id}"
 )
--- run/user-id-request-action.sh
+++ run/user-id-request-action.sh
@@ -3,5 +3,14 @@
   export GH_CONFIG_DIR="${HOME}/data/config/git"
   source "${HOME}/data/config/.gitattributes"
   gh loguser
-  gh workflow run "user-id-request-action"
+  gh workflow run "user-id-request-action".yml
+  gh run list --workflow="user-id-request-action".yml
+  gh run watch -i 1 2>/dev/null
+  run_info="$( gh run list --workflow=intro.yml --limit 1 | grep -v "^STATUS" | grep workflow_dispatch )"
+  status="$( echo "${run_info}" | awk '{print $1}' )"
+  rc="$( echo "${run_info}" | awk '{print $2}' )"
+  id="$( echo "${run_info}" | awk '{print $7}' )"
+  echo "status=${status}, rc=${rc} id=${id}"
+  gh run view "${id}"
+  open "https://github.com/j5pu/test-actions/actions/runs/${id}"
 )
