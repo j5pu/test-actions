6774694af34f5afb8df59d517b6337edd0aa9ee8
José Antonio Puértolas Montañés <63794670+jose-nferx@users.noreply.github.com>
2021-08-17 05:25:13 +0200

    'auto commit'

--- run/step-output-and-env.sh
+++ run/step-output-and-env.sh
@@ -3,12 +3,12 @@
   export GH_CONFIG_DIR="${HOME}/data/config/git"
   source "${HOME}/data/config/.gitattributes"
   gh loguser
-  gh workflow run "outputs-and-env".yml
+  gh workflow run "step-output-and-env".yml
   sleep 2
-  run_info="$( gh run list --workflow=outputs-and-env.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
+  run_info="$( gh run list --workflow=step-output-and-env.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
   id="$( echo "${run_info}" | awk '{print $7}' )"
   gh run watch -i 1 "${id}"
-  run_info="$( gh run list --workflow=outputs-and-env.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
+  run_info="$( gh run list --workflow=step-output-and-env.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
   status="$( echo "${run_info}" | awk '{print $1}' )"
   rc="$( echo "${run_info}" | awk '{print $2}' )"
   echo "status=${status}, rc=${rc} id=${id}"
