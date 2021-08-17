094570d8b2d360a9defac6f66851cb5c7e6b0e2d
José Antonio Puértolas Montañés <63794670+jose-nferx@users.noreply.github.com>
2021-08-17 05:10:47 +0200

    'auto commit'

--- run/context-dump.sh
+++ run/context-dump.sh
@@ -3,12 +3,12 @@
   export GH_CONFIG_DIR="${HOME}/data/config/git"
   source "${HOME}/data/config/.gitattributes"
   gh loguser
-  gh workflow run "context".yml
+  gh workflow run "context-dump".yml
   sleep 2
-  run_info="$( gh run list --workflow=context.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
+  run_info="$( gh run list --workflow=context-dump.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
   id="$( echo "${run_info}" | awk '{print $7}' )"
   gh run watch -i 1 "${id}"
-  run_info="$( gh run list --workflow=context.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
+  run_info="$( gh run list --workflow=context-dump.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
   status="$( echo "${run_info}" | awk '{print $1}' )"
   rc="$( echo "${run_info}" | awk '{print $2}' )"
   echo "status=${status}, rc=${rc} id=${id}"
