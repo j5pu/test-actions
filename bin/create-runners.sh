7f3e21bce90b3c519d56df946da6aa70e8cbb0a9
José Antonio Puértolas Montañés <63794670+jose-nferx@users.noreply.github.com>
2021-08-17 03:07:31 +0200

    'auto commit'

--- bin/create-runners.sh
+++ bin/create-runners.sh
@@ -14,7 +14,16 @@ gall.sh
   export GH_CONFIG_DIR="\${HOME}/data/config/git"
   source "\${HOME}/data/config/.gitattributes"
   gh loguser
-  gh workflow run "${name}"
+  gh workflow run "${name}".yml
+  gh run list --workflow="${name}".yml
+  gh run watch -i 1 2>/dev/null
+  run_info="\$( gh run list --workflow=intro.yml --limit 1 | grep -v "^STATUS" | grep workflow_dispatch )"
+  status="\$( echo "\${run_info}" | awk '{print \$1}' )"
+  rc="\$( echo "\${run_info}" | awk '{print \$2}' )"
+  id="\$( echo "\${run_info}" | awk '{print \$7}' )"
+  echo "status=\${status}, rc=\${rc} id=\${id}"
+  gh run view "\${id}"
+  open "https://github.com/j5pu/test-actions/actions/runs/\${id}"
 )
 EOF
     chmod +x ./run/"${name}".sh
