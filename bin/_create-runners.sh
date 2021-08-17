3f3887078d97f29cda553e01ef7e8db85e9c45fa
José Antonio Puértolas Montañés <63794670+jose-nferx@users.noreply.github.com>
2021-08-17 08:07:38 +0200

    'auto commit'

--- bin/_create-runners.sh
+++ bin/_create-runners.sh
@@ -4,7 +4,29 @@ gall.sh
 (
   cd "${dir}/.." || exit 1
   cwd="$( pwd )"
-  pre-commit sample-config
-  pre-commit install --install-hooks
-  pre-commit autoupdate
+  rm -f ./run/*.sh
+  for i in ./.github/workflows/*.yml; do
+    file="${i##*/}"
+    name="${file%.*}"
+    cat <<EOF >./run/"${name}".sh
+#!/usr/bin/env bash
+(
+  export GH_CONFIG_DIR="\${HOME}/data/config/git"
+  source "\${HOME}/data/config/.gitattributes"
+  gh loguser
+  gh workflow run "${name}".yml
+  sleep 2
+  run_info="\$( gh run list --workflow=${name}.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
+  id="\$( echo "\${run_info}" | awk '{print \$7}' )"
+  gh run watch -i 1 "\${id}"
+  run_info="\$( gh run list --workflow=${name}.yml --limit 1 | grep -v '^STATUS' | grep workflow_dispatch )"
+  status="\$( echo "\${run_info}" | awk '{print \$1}' )"
+  rc="\$( echo "\${run_info}" | awk '{print \$2}' )"
+  echo "status=\${status}, rc=\${rc} id=\${id}"
+  gh run view "\${id}"
+  open "https://github.com/j5pu/test-actions/actions/runs/\${id}"
+)
+EOF
+    chmod +x ./run/"${name}".sh
+  done
 )
